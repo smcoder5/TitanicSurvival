64080c617864607bad3ed31155417e24a38e45f9
smcoder5 <116910466+smcoder5@users.noreply.github.com>
2024-02-09 00:11:00 +0530

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -2,9 +2,12 @@ import streamlit as s
 import pandas as p
 
 
+a=p.read_csv('titanic_data.csv')
+
 if __name__ == "__main__": 
     s.set_page_config(page_title='Titanic Survival',layout='wide') 
     s.title("Titanic Survival Prediction") 
+    s.table(a)
     cl=s.columns([1,3,1]) 
     with cl[0]:
         gen=s.selectbox("Enter Gender",['Male','Female']) 
